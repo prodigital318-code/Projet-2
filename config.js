c53f85471d35398b871a10bfd32eef9d628c8a4f
prodigital318-code <prodigital318@gmail.com>
2025-09-29 20:17:23 +0000

    config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 // config.js - Configuration Supabase
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
 
-const SUPABASE_URL = 'https://sxhtqikqklriyjttnyme.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndhYnVoYXB1Zm9hcmpwZmxxY3NvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTg5ODMyOTgsImV4cCI6MjA3NDU1OTI5OH0.bNRvqHfSmk5-PVnUzVfxgU_FWpaKpqgx-VF1_4ec6v4';
+const SUPABASE_URL = 'https://gopangnvdrsrdrrgbste.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdvcGFuZ252ZHJzcmRycmdic3RlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTkxNzY4Niwib3hwIjoyMDc0NzUyNjg2fQ.R8iyquMXsaT5oTK-puTu8uPeifq6co4gKVV4FiBq1p0';
 
 // Création du client Supabase
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
